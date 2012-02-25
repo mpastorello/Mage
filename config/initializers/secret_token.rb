9e7ec126efb271cdb28717daac0f5c2f76ee3b32
Marco Pastorello <marko.pastorello@gmail.com>
2012-02-25 09:57:19 +0100

    first Commit
    Signed-off-by: Marco Pastorello <marko.pastorello@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mage::Application.config.secret_token = '4b23c313da47bcaa17a2b98706c8cfbc89fa0be2b50e90db2c0d8a54bfe4addcc3002305e578e144e46994b5f33d95a9e291ba54bb148fc1bb0b8883038e89b9'
